46a5d6100bb50d63ce35aec91a63d56780040f2c
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-05-06 17:37:50 -0700

    sending information over network working

--- client.c
+++ client.c
@@ -8,15 +8,23 @@ int create_client_socket(int port) {
    	struct sockaddr_in s_address;
    	s_address.sin_family = AF_INET;
     	s_address.sin_addr.s_addr = INADDR_ANY;
-    	s_address.sin_port = htons(port);
+    	s_address.sin_port = port;
         c_sock = socket(AF_INET, SOCK_STREAM, 0);
         connect(c_sock, (struct sockaddr *)&s_address, sizeof(s_address));
    	return c_sock;
 }
 
 
+void send_msg(int sock) {
+	char buffer[100];
+	printf("Enter your message: ");
+	fgets(buffer, sizeof(buffer), stdin);
+	send(sock, buffer, sizeof(buffer), 0);
+	printf("Message sent\n");
+}
+
 int main() {
 	int c_sock = create_client_socket(64209);
-	printf("connected client");
+	close(c_sock);
 	return 0;
 }
--- server.c
+++ server.c
@@ -9,7 +9,7 @@ int create_server_sock(int port) {
 	struct sockaddr_in s_addy;
 	s_addy.sin_family = AF_INET;
 	s_addy.sin_addr.s_addr = INADDR_ANY;
-	s_addy.sin_port = htons(port);
+	s_addy.sin_port = port;
 	bind(sock, (struct sockkaddr*)&s_addy, sizeof(s_addy));
 	return sock;
 }
@@ -17,16 +17,24 @@ int create_server_sock(int port) {
 int connect_2_client(int s_sock)
 {
      listen(s_sock, 5);
-     printf("Waiting for client to finish setup and connect\n!");
      int c_sock = accept(s_sock, NULL, NULL);
-     printf("Client connected\n");
      return c_sock;
+}
+
+char* receive_msg(int sock) {
+	char buffer[1000];
+	recv(sock, buffer, sizeof(buffer), 0);
+	char *ret = malloc(sizeof(buffer) * sizeof(char));
+	*ret = buffer;
+	printf("%s\n", buffer);
+	return ret;
 }
 
 int main() {
 	int s_sock = create_server_sock(64209);
 	int c_sock = connect_2_client(s_sock);
-
+	close(c_sock);
+	close(s_sock);
 	return 0;
 
 }
