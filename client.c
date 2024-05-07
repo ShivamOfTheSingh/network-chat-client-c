747d51f576ab1733ae7ea79eb790677b093b5bd5
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-05-06 18:06:10 -0700

    netowrking done

--- client.c
+++ client.c
@@ -2,16 +2,27 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 int create_client_socket(int port) {
 	int c_sock;
    	struct sockaddr_in s_address;
    	s_address.sin_family = AF_INET;
-    	s_address.sin_addr.s_addr = INADDR_ANY;
-    	s_address.sin_port = port;
-        c_sock = socket(AF_INET, SOCK_STREAM, 0);
-        connect(c_sock, (struct sockaddr *)&s_address, sizeof(s_address));
-   	return c_sock;
+	s_address.sin_port = htons(port);
+	inet_pton(AF_INET, "10.110.234.245", &s_address.sin_addr);
+
+	while (1) {
+		c_sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (connect(c_sock, (struct sockaddr *)&s_address, sizeof(s_address)) < 0) {
+			printf("Waiting on server, retrying in 5 seconds\n");
+			close(c_sock);
+			sleep(5);
+		}
+		else {
+			printf("Connected to client!\n");
+			return c_sock;
+		}
+	}
 }
 
 
@@ -25,6 +36,7 @@ void send_msg(int sock) {
 
 int main() {
 	int c_sock = create_client_socket(64209);
+	send_msg(c_sock);
 	close(c_sock);
 	return 0;
 }
--- server.c
+++ server.c
@@ -3,36 +3,40 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
 
 int create_server_sock(int port) {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in s_addy;
 	s_addy.sin_family = AF_INET;
-	s_addy.sin_addr.s_addr = INADDR_ANY;
-	s_addy.sin_port = port;
+	s_addy.sin_addr.s_addr = inet_addr("10.110.234.245");
+	s_addy.sin_port = htons(port);
 	bind(sock, (struct sockkaddr*)&s_addy, sizeof(s_addy));
 	return sock;
 }
 
-int connect_2_client(int s_sock)
-{
+int connect_2_client(int s_sock) {
      listen(s_sock, 5);
      int c_sock = accept(s_sock, NULL, NULL);
      return c_sock;
 }
 
 char* receive_msg(int sock) {
-	char buffer[1000];
-	recv(sock, buffer, sizeof(buffer), 0);
-	char *ret = malloc(sizeof(buffer) * sizeof(char));
-	*ret = buffer;
-	printf("%s\n", buffer);
+	char buffer[1000] = {0};
+	int bytes_received = recv(sock, buffer, sizeof(buffer) - 1, 0);
+	buffer[bytes_received] = '\0';
+	char *ret = malloc((bytes_received + 1) * sizeof(char));
+	strcpy(ret, buffer);
 	return ret;
 }
 
 int main() {
 	int s_sock = create_server_sock(64209);
 	int c_sock = connect_2_client(s_sock);
+
+	printf("%s\n", receive_msg(c_sock));
+
 	close(c_sock);
 	close(s_sock);
 	return 0;
