57a74c8f73e8cd31272ddadc0ace5b76b27a9106
ShivamOfTheSingh <shivam2003.singh@gmail.com>
2024-05-15 20:10:18 -0700

    working ui on serverside

--- client.c
+++ client.c
@@ -1,43 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int create_client_socket(int port) {
-	int c_sock;
-	struct sockaddr_in s_address;
-	s_address.sin_family = AF_INET;
-	s_address.sin_addr.s_addr = INADDR_ANY;
-	s_address.sin_port = htons(port);
+#define MSG_SIZE 100
+#define USRNM_SIZE 20
 
-	while (1) {
-		c_sock = socket(AF_INET, SOCK_STREAM, 0);
-		if (connect(c_sock, (struct sockaddr*)&s_address, sizeof(s_address)) < 0) {
-			printf("Waiting on server, retrying in 5 seconds\n");
-			close(c_sock);
-			sleep(5);
-		}
-		else {
-			printf("Connected to server!\n");
-			return c_sock;
-		}
-	}
+int c_sock;
+
+void handle_exit(int sig) {
+    char exit_msg = 27;
+    send(c_sock, &exit_msg, sizeof(char), 0);
+    close(c_sock);
+    printf("\nDisconnected from server\n");
+    exit(0);
 }
 
-void send_msg(int sock) {
-	char *buffer = malloc(100 * sizeof(char));
-	printf("Enter your message: ");
-	fgets(buffer, sizeof(buffer), stdin);
-	printf("%d", sizeof(buffer));
-	send(sock, buffer, sizeof(buffer), 0);
-	printf("Message sent\n");
+int create_client_socket(int port) {
+    struct sockaddr_in s_address;
+    s_address.sin_family = AF_INET;
+    s_address.sin_addr.s_addr = INADDR_ANY;
+    s_address.sin_port = htons(port);
+
+    while (1) {
+        c_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (connect(c_sock, (struct sockaddr*)&s_address, sizeof(s_address)) < 0) {
+            printf("Waiting on server, retrying in 5 seconds\n");
+            close(c_sock);
+            sleep(5);
+        } else {
+            printf("Connected to server!\n");
+            return c_sock;
+        }
+    }
+}
+
+int send_msg(int sock) {
+    char *buffer = malloc(MSG_SIZE * sizeof(char));
+    printf("Enter your message: ");
+    fgets(buffer, MSG_SIZE, stdin);
+    buffer[strlen(buffer) - 1] = '\0';
+    send(sock, buffer, strlen(buffer), 0);
+    free(buffer);
+    return 0;
 }
 
 int main() {
-	int c_sock = create_client_socket(64209);
-	send_msg(c_sock);
-	close(c_sock);
-	return 0;
-}
+    signal(SIGINT, handle_exit);
+    c_sock = create_client_socket(64209);
 
+    char *username = malloc(USRNM_SIZE * sizeof(char));
+    printf("Enter your username: ");
+    fgets(username, USRNM_SIZE, stdin);
+    username[strlen(username) - 1] = '\0';
+    send(c_sock, username, strlen(username), 0);
+    free(username);
+
+    while (1) {
+        send_msg(c_sock);
+    }
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -5,6 +5,8 @@
 #include <netinet/in.h>
 #include <pthread.h>
 
+# define MSG_SIZE 100
+
 int create_server_sock(int port) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in s_addy;
@@ -22,17 +24,28 @@ int connect_2_client(int s_sock) {
 }
 
 char* receive_msg(int sock) {
-    char *buffer = malloc(100 * sizeof(char));
-    int bytes_received = recv(sock, buffer, 100, 0);
-    buffer[bytes_received - 3] = '\0';
+    char *buffer = malloc(MSG_SIZE * sizeof(char));
+    int bytes_received = recv(sock, buffer, MSG_SIZE, 0);
+    buffer[bytes_received] = '\0';
     return buffer;
 }
 
 void *client_stream(void *sock) {
     int c_sock = *(int *)sock;
-    char *buffer = receive_msg(c_sock);
-    printf("%s\n", buffer);
-    free(buffer);
+    char *buffer;
+    char *username = receive_msg(c_sock);
+    printf("%s has joined the chat.\n", username);
+
+    while (1) {
+        buffer = receive_msg(c_sock);
+	if (*buffer == 27) {
+	  printf("%s has left.\n", username);
+	  break;
+	}
+        printf("%s: %s\n", username, buffer);
+        free(buffer);
+    }
+    free(username);
     close(c_sock);
 }
 
@@ -40,16 +53,14 @@ int main() {
     int s_sock = create_server_sock(64209);
     close(s_sock);
     s_sock = create_server_sock(64209);
-
+    printf("Server has started! Waiting for clients\n");
     int clients = 0;
     pthread_t threads[10];
 
     while (clients < 10) {
-        printf("Waiting on client %d\n", clients);
         int t_sock = connect_2_client(s_sock);
-        printf("Client %d connected\n", clients);
         pthread_create(&threads[clients], NULL, client_stream, (void *)&t_sock);
         ++clients;
-
     }
 }
+
